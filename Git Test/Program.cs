637a09bc5b7bb69a81a24c056af4d56d61cf3bf5
Christopher Miller <chrdmill@ut.utm.edu>
2020-01-16 14:22:03 -0600

    It works now

--- Git Test/Program.cs
+++ Git Test/Program.cs
@@ -10,6 +10,30 @@ namespace Git_Test
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("I am thinking of a number between 0 and 100");
+            Random random = new Random();
+            int NumToGuess = random.Next(0, 100);
+            int UserGuess = 0;
+            while (UserGuess != NumToGuess)
+            {
+                Console.Write("Enter Guess Here: ");
+                UserGuess = Convert.ToInt32(Console.ReadLine());
+
+                if (UserGuess < NumToGuess)
+                {
+                    Console.WriteLine("Too Low");
+
+                }
+                else if (UserGuess > NumToGuess)
+                {
+                    Console.WriteLine("Too High");
+                }
+                else
+                {
+                    Console.WriteLine("You Have Guessed Correctly!");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
