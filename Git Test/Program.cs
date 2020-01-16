9c7251d7120682f23847145322df46289f434deb
Christopher Miller <chrdmill@ut.utm.edu>
2020-01-16 14:33:10 -0600

    The program now allows you to selects the bounds of the number game

--- Git Test/Program.cs
+++ Git Test/Program.cs
@@ -10,9 +10,16 @@ namespace Git_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I am thinking of a number between 0 and 100");
+            Console.Write("Enter Number A: ");
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Number B: ");
+            int Num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("I am thinking of a number between ");
+            Console.Write(Num1);
+            Console.Write(" and ");
+            Console.WriteLine(Num2);
             Random random = new Random();
-            int NumToGuess = random.Next(0, 100);
+            int NumToGuess = random.Next(Num1, Num2);
             int UserGuess = 0;
             while (UserGuess != NumToGuess)
             {
